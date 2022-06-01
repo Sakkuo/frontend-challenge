9e92875211f02357ae9394e270fb73b46591fe6b
Ilya <il.kii@mail.ru>
2022-06-01 06:24:00 +0200

    Added lazy load

--- src/components/mainPage/ImageContainer/ImageContainer.tsx
+++ src/components/mainPage/ImageContainer/ImageContainer.tsx
@@ -1,24 +1,55 @@
-import React, { useState } from "react";
+import axios from "axios";
+import { AnyArray } from "immer/dist/internal";
+import React, { useEffect, useState } from "react";
+import { IImage } from "../../../models/IImage";
 import { imageAPI } from "../../../services/ImageService";
 import ImageItem from "../ImageItem/ImageItem";
 import "./ImageContainer.css";
 
 const ImageContainer: React.FC = () => {
-    const [limit, setLimit] = useState(15);
+    const [limit, setLimit] = useState(20);
+    const [page, setPage] = useState(1)
     const {
         data: images,
         isLoading,
         error,
-    } = imageAPI.useFetchAllImagesQuery(limit);
+        isSuccess
+    } = imageAPI.useFetchAllImagesQuery([limit, page])
+    const [newImages, setNewImages] = useState<AnyArray>([])
 
-    console.log(images)
+
+    useEffect(() => {
+        if(isSuccess) {
+            setNewImages([...newImages,...images])
+        }
+    }, [isSuccess === true])
+
+
+
+    useEffect(() => {
+        document.addEventListener('scroll', scrollHandler)
+
+        return function () {
+            document.removeEventListener('scroll', scrollHandler)
+        }
+    }, [])
+
+
+    const scrollHandler = (e: any) => {
+        setTimeout(() => {
+            if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 150) {
+                setPage(prevState => prevState + 1)
+            }
+        }, 1000)
+    }
+
 
     return (
         <div className="image__list">
             {isLoading && <div>Идёт загрузка котиков</div>}
             {error && <div>Произошла ошибка при загрузке котиков(((</div>}
-            {images?.map((image) => (
-                <ImageItem image={image} key={image.id} />
+            {newImages?.map((image: IImage) => (
+                <ImageItem image={image} key={image.id+Math.random()*20} />
             ))}
         </div>
     );
--- src/services/ImageService.ts
+++ src/services/ImageService.ts
@@ -6,11 +6,12 @@ export const imageAPI = createApi({
     tagTypes: ['Image'],
     baseQuery: fetchBaseQuery({baseUrl: 'https://api.thecatapi.com/v1/images'}),
     endpoints: (build) => ({
-        fetchAllImages: build.query<IImage[], number>({
-            query: (limit = 15, mime_types: string[] = ['jpg', 'png'] ) => ({
+        fetchAllImages: build.query<IImage[], number[]>({
+            query: (limitPage: number[] = [20, 1] , mime_types: string[] = ['jpg', 'png']) => ({
                 url: '/search',
                 params: {
-                    limit: limit,
+                    limit: limitPage[0],
+                    page: limitPage[1],
                     mime_types: mime_types
                 }
             }),
